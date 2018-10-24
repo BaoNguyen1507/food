657d20620bce0a51f279883ed051c3ba4b2683e6
ThanhVo <thanh.vo@zinimedia.com>
2018-10-24 10:26:42 +0700

    AddRoles

---
+++ api/models/Role.js
@@ -0,0 +1,30 @@
+/**
+ * @copyright 2017 @ ZiniMediaTeam
+ * @author brianvo
+ * @create 2017/10/23 01:05
+ * @update 2017/10/23 01:05
+ * @file api/models/Subject.js
+ * @description :: Subject model.
+ */
+
+module.exports = {
+  attributes: {
+    title: {
+      type: 'string',
+      required: true
+    },
+    description: {
+      type: 'string'
+    },
+    permissions: {
+      type: 'json',
+      required: true
+      //['Album':{'view':true,'edit':false,'delete':false}]
+    },
+    status: { //Integer {"TRASH":,"DRAFT":,"PUBLISH":, SCHEDULE:}
+      type: 'number',
+      isIn: [sails.config.custom.STATUS.TRASH,sails.config.custom.STATUS.DRAFT,sails.config.custom.STATUS.PUBLISH],
+      defaultsTo: sails.config.custom.STATUS.DRAFT
+    }
+  }
+};
