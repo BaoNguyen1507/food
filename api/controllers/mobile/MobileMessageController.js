dbdc24772e49a1b22efcea9f4226feb118d3667d
Le Truong An <letruongan@gmail.com>
2019-02-28 11:18:33 +0700

    refactor code message api

--- api/controllers/mobile/MobileMessageController.js
+++ api/controllers/mobile/MobileMessageController.js
@@ -18,84 +18,64 @@ const moment = require('moment');
 module.exports = {
   getGroupMessage: async (req, res) => {
     let params = req.allParams();
-    let classID = params.classId ? params.classId : '';
-    let parentID = params.parentId ? params.parentId : '';
-    let teacherID = params.teacherId ? params.teacherId : '';
+    let classID = params.classId;
+    let parentID = params.parentId;
+    let teacherID = params.teacherId;
 
-    if (parentID != '' && classID != '') {
-      let listGroupMessage = await MessageService.find({
-        classes: classID,
-        parent: parentID
-      });
-      return res.ok({
-        status: 200,
-        data: listGroupMessage
-      });
-    } else if (teacherID != '' && classID != '') {
-      let listGroupMessage = await MessageService.find({
-        classes: classID,
-        teacher: teacherID
-      });
-      return res.ok({
-        status: 200,
-        data: listGroupMessage
-      });
-    } else if (teacherID == '' && classID != '') {
-      return res.ok({
+    //check params exists
+    //class Id
+    if (classID == undefined) {
+      return res.badRequest({
         status: 400,
-        data: 'Missing Teacher ID'
+        message: MessageError.ERR_CLASS_ID_REQUIRED
       });
-    } else if (parentID == '' && classID != '') {
-      return res.ok({
+    } else if (teacherID == undefined && parentID == undefined) {
+      //teacher or parent id
+      return res.badRequest({
         status: 400,
-        data: 'Missing Parent ID'
-      });
-    } else if (classID == '' && parentID != '') {
-      return res.ok({
-        status: 400,
-        data: 'Missing Class ID'
-      });
-    } else if (classID == '' && classID != '') {
-      return res.ok({
-        status: 400,
-        data: 'Missing Class ID'
-      });
-    } else {
-      return res.ok({
-        status: 400,
-        data: 'Need input data ID'
+        message: MessageError.ERR_TEACHER_PARENT_ID_REQUIRED
       });
     }
+
+    let listGroupMessage = await MessageService.find({
+      classes: classID,
+      parent: parentID
+    });
+
+    for (let i = 0; i < listGroupMessage.length; i++) {
+      listGroupMessage[i].latestMessages = await MessageDataService.find({
+        message: listGroupMessage[i].id
+      }).sort('createdAt DESC');
+    }
+
+    return res.ok({
+      status: 200,
+      data: listGroupMessage
+    });
   },
   storeMessageData: async (req, res) => {
     let params = req.allParams();
-    let userId = params.userId ? params.userId : '';
-    let txtMessage = params.txtMessage ? params.txtMessage : '';
-    let message = params.messageId ? params.messageId : '';
-    if (userId == '' || message == '') {
-      return res.badRequest('Missing data to add');
-    } else {
-      let dataLogs = {
-        user: userId,
-        txtMessage: txtMessage
-      };
-      let data = {
-        message: message,
-        dataLogs
-      };
-      let dataObj = await MessageDataService.add(data);
-      return res.ok({
-        status: 200,
-        data: dataObj
-      });
-    }
+    let userId = params.userId;
+    let txtMessage = params.txtMessage;
+    let message = params.messageId;
+
+    let dataLogs = {
+      user: userId,
+      txtMessage: txtMessage
+    };
+    let data = {
+      message: message,
+      dataLogs
+    };
+    let dataObj = await MessageDataService.add(data);
+    return res.ok({
+      status: 200,
+      data: dataObj
+    });
   },
   showDataMessage: async (req, res) => {
     let params = req.allParams();
-    let message = params.messageId ? params.messageId : '';
-    if (message == '') {
-      return res.badRequest('Missing message group id');
-    }
+    let message = params.messageId;
 
     let listMessage = await MessageDataService.find({ message: message });
     let data = [];
@@ -132,11 +112,7 @@ module.exports = {
     let params = req.allParams();
     let msgId = params.messageId;
 
-    let editObj = await MessageService.edit(
-      {
-        id: msgId
-      },
-      {
+    let editObj = await MessageService.edit({ id: msgId },{
         lastSeen: moment().valueOf()
       }
     );
